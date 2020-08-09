edb38c978ef8f164ac2c188190172ef9ca91cb90
Jack Meredith <jackmered@gmail.com>
2020-08-09 16:57:32 +0100

    updated deps

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # commander-gulp
+
+Front end development boilerplate gulpfile
+
+Inlcuding:
+
+- SASS
+- postcss
+- babel
+- webpack
+- stylelint
+- ESLINT
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,4 @@
-import {
-  src, dest, parallel, series, watch,
-} from 'gulp';
+import { src, dest, parallel, series, watch } from 'gulp';
 import webpackStream from 'webpack-stream';
 import handlebarsLayouts from 'handlebars-layouts';
 import del from 'del';
@@ -16,10 +14,11 @@ const plugins = gulpLoadPlugins();
  * Assets
  * Moves all child folders to root of dist
  */
-const assets = () => src('./src/assets/**/*.*', { base: './src/assets' })
-  .pipe(plugins.if(isProduction, plugins.imagemin()))
-  .pipe(plugins.connect.reload())
-  .pipe(dest('./dist'));
+const assets = () =>
+  src('./src/assets/**/*.*', { base: './src/assets' })
+    .pipe(plugins.if(isProduction, plugins.imagemin()))
+    .pipe(plugins.connect.reload())
+    .pipe(dest('./dist'));
 
 /**
  * Templates
@@ -27,7 +26,8 @@ const assets = () => src('./src/assets/**/*.*', { base: './src/assets' })
  */
 const templates = () => {
   const srcDir = './src/templates';
-  const hbStream = plugins.hb()
+  const hbStream = plugins
+    .hb()
     .partials(`${srcDir}/partials/components/**/*.{hbs,js}`)
     .partials(`${srcDir}/partials/layouts/**/*.{hbs,js}`)
     .helpers(handlebarsLayouts)
@@ -45,56 +45,60 @@ const templates = () => {
 
 /**
  * Styles
- * PostCSS, CSSO & Sourcemaps
+ * SASS, PostCSS, CSSO & Sourcemaps
  */
 plugins.sass.compiler = dartSass;
 
-const styles = () => src('./src/styles/app.scss')
-  .pipe(plugins.sourcemaps.init())
-  .pipe(plugins.sass().on('error', plugins.sass.logError))
-  .pipe(plugins.if(isProduction, plugins.csso()))
-  .pipe(plugins.postcss())
-  .pipe(plugins.sourcemaps.write('./'))
-  .pipe(plugins.connect.reload())
-  .pipe(dest('./dist/styles'));
+const styles = () =>
+  src('./src/styles/app.scss')
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass().on('error', plugins.sass.logError))
+    .pipe(plugins.if(isProduction, plugins.csso()))
+    .pipe(plugins.postcss())
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(plugins.connect.reload())
+    .pipe(dest('./dist/styles'));
 
 /**
  * Stylelint
  * Checks all stylesheets to ensure they are formatted correctly
  */
-const stylelint = () => src('./src/styles/**/*.scss')
-  .pipe(plugins.stylelint({
-    reporters: [
-      { formatter: 'string', console: true },
-    ],
-  }));
+const stylelint = () =>
+  src('./src/styles/**/*.scss').pipe(
+    plugins.stylelint({
+      reporters: [{ formatter: 'string', console: true }],
+    })
+  );
 
 /**
  * Scripts
  * Transpiles es6 javascript and bundles into into dist/scripts folder, also minifies for production
  */
-const scripts = () => src('./src/scripts/app.js')
-  .pipe(webpackStream(webpackConfig, webpack))
-  .pipe(plugins.connect.reload())
-  .pipe(dest('./dist/scripts'));
+const scripts = () =>
+  src('./src/scripts/app.js')
+    .pipe(webpackStream(webpackConfig, webpack))
+    .pipe(plugins.connect.reload())
+    .pipe(dest('./dist/scripts'));
 
 /**
  * ESLint
  * Checks all javascript is formatted correctly
  */
-const eslint = () => src(['src/scripts/**/*.js'])
-  .pipe(plugins.eslint())
-  .pipe(plugins.eslint.format())
-  .pipe(plugins.eslint.failAfterError());
+const eslint = () =>
+  src(['src/scripts/**/*.js'])
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failAfterError());
 
 /**
  * Serve
  */
-const serve = () => plugins.connect.server({
-  root: 'dist',
-  livereload: true,
-  host: '0.0.0.0',
-});
+const serve = () =>
+  plugins.connect.server({
+    root: 'dist',
+    livereload: true,
+    host: '0.0.0.0',
+  });
 
 /**
  * Watch
@@ -110,9 +114,7 @@ const watcher = () => {
 /**
  * Build all
  */
-export const build = parallel(
-  assets, templates, series(stylelint, styles), series(eslint, scripts),
-);
+export const build = parallel(assets, templates, series(stylelint, styles), series(eslint, scripts));
 
 /**
  * Clean
